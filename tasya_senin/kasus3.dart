0b918eda450ca1dd6ccc5b5db6c4280bb22a78d1
fbrianiacaa <113565624+tasyafbriani@users.noreply.github.com>
2023-10-16 11:15:46 +0700

    Update kasus3.dart

--- tasya_senin/kasus3.dart
+++ tasya_senin/kasus3.dart
@@ -1,17 +1,37 @@
+
 import 'dart:io';
+void main(){
+  stdout.write('nama pelanggan:');
+  var nama = stdin.readLineSync();
 
-void main() {
-  stdout.write('Nama Pelanggan:');
-  int p = int.parse(stdin.readLineSync()!);
-  stdout.write('Nama Barang:');
-  int b = int.parse(stdin.readLineSync()!);
-  stdout.write('Jumlah:');
-  int j = int.parse(stdin.readLineSync()!);
-  stdout.write('Harga:');
-  int p = int.parse(stdin.readLineSync()!);
+  stdout.write('nama barang :');
+  var barang = stdin.readLineSync();
 
+  stdout.write('jumlah barang: ');
+  int jumlah= int.parse(stdin.readLineSync()!);
 
+  stdout.write('harga barang: ');
+  int harga= int.parse(stdin.readLineSync()!);
+  var diskon;
+  num bayar = (jumlah*harga);
+ 
+    if (bayar >=500000)
+    {
+      diskon= 20;
+    }else 
+    if(bayar>=200000 ){
+      diskon =15;
+    }else
+    if(bayar>100){
+      diskon=2;
+    }
+    //print('diskon :$diskon');
+   double jmdiskon=(diskon*bayar)/100;
+    num jmbayar=bayar-jmdiskon;
+    print('jumlah bayar :$jmbayar');
+  stdout.write('uang yang dibayar: ');
+  int pembayaran= int.parse(stdin.readLineSync()!);  
 
-
-  
+  num kembali= pembayaran - jmbayar ;
+  print('kembalian :$kembali');
 }
